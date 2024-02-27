545b4e25d5edb9183318accbea9033c5558c3ed8
Emilio Primic <ephraimicarus@gmail.com>
2024-02-27 16:28:57 +0100

    key

--- unitas/app/components/navbar.tsx
+++ unitas/app/components/navbar.tsx
@@ -20,7 +20,7 @@ export default function NavBar() {
       <div className="text-whitenitas flex gap-x-[20px] px-[10px] sm:gap-x-[20px] sm:px-[10px] md:px-[25px] lg:px-[100px]">
         <div className="border-whitenitas border-2 sm:flex">
           {navLinks.map((item, index) => (
-            <Link href={item.route}>
+            <Link href={item.route} key={index}>
               <div
                 className="whitespace-nowrap text-whitenitas bg-bluenitas px-4 py-2 md:px-8 lg:px-12 lg:py-6"
                 key={index}
