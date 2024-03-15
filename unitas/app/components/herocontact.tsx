81cdab5a4af56a6d2299637c9eb55dbf07d57453
Emilio Primic <ephraimicarus@gmail.com>
2024-03-15 12:43:06 +0100

    form disabled, company address added

--- unitas/app/components/herocontact.tsx
+++ unitas/app/components/herocontact.tsx
@@ -1,37 +1,39 @@
 export default function HeroContact() {
   return (
-    <div className="pt-6">
-      <div className="px-[20px]">
-        <h1 className="text-center text-[24px] text-bluenitas leading-[40px] font-semibold">
-          For any enquiries, please fill out and submit the form below
-        </h1>
-       
+    <div>
+      <div className="pt-6 mt-6 text-center flex justify-center items-center w-full">
+        <div className="w-2/3">
+          <h1 className="text-center text-[24px] text-bluenitas leading-[40px] font-semibold">
+            For any enquiries, please feel free to contact us by email or via
+            telephone
+          </h1>
+        </div>
       </div>
-      <div className="w-full flex justify-center p-[50px]">
-        <form>
-          <input
-            type="text"
-            placeholder="Enter your name..."
-            className="border-4 border-bluenitas/50"
-          />
-          <br />
-          <br />
-          <textarea
-            rows={5}
-            placeholder="Please write your enquiry here..."
-            className="border-4 border-bluenitas/50 w-full"
-          ></textarea>
-          <br />
-
-          <button className="bg-bluenitas/50 hover:bg-whitenitas text-whitenitas hover:text-bluenitas font-bold py-2 px-4 rounded-full border-2 border-bluenitas/50">
-            Submit
-          </button>
-        </form>
+      <div className="px-6 pt-6 my-4 text-center flex justify-center items-center w-full">
+        <div className="w-2/3 text-bluenitas">
+          <h1 className="text-center text-[24px] leading-[40px] font-semibold">
+            Email
+          </h1>
+          <p className="text-[16px] mt-2">info@unitaspaper.com</p>
+        </div>
       </div>
-      <p className="text-center py-4 px-2">
-        Email: info@unitaspaper.com <br />
-          Mobile: +387 66 236 726<br/>
-        </p>
+      <div className="px-6 pt-6 my-4 text-center flex justify-center items-center w-full">
+        <div className="w-2/3 text-bluenitas">
+          <h1 className="text-center text-[24px] leading-[40px] font-semibold">
+            Mobile
+          </h1>
+          <p className="text-[16px] mt-2">+387 66 236 726</p>
+        </div>
+      </div>
+      <div className="px-6 pt-6 my-4 text-center flex justify-center items-center w-full">
+        <div className="w-2/3 text-bluenitas">
+          <h1 className="text-center text-[24px] leading-[40px] font-semibold">
+            Company address
+          </h1>
+          <p className="text-[16px] mt-2">Unitas d.o.o Teslic<br/> Sime Lozanica bb<br/>Bosnia and Herzegovina</p>
+        </div>
+      </div>
     </div>
+    
   );
 }
