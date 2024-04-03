739bbdc0076f4d6da09c036fabea55f89097addd
Emilio Primic <ephraimicarus@gmail.com>
2024-04-03 15:36:29 +0200

    slideshow

--- unitas/app/page.tsx
+++ unitas/app/page.tsx
@@ -2,14 +2,15 @@
 import Hero from "./components/hero";
 import NavBar from "./components/navbar";
 import Footer from "./components/footer";
+import Slideshow from "./components/slide"
 
 export default function Home() {
   return (
     <>
       <NavBar/>
       <Hero/>
+      <Slideshow/>
       <Footer/>
-
     </>
   );
 }
