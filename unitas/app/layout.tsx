21d2ad71d39cb8844902e82aded17452e3ea4182
ephraimicarus <eprimic123@outlook.com>
2025-07-14 10:26:35 +0200

    test

--- unitas/app/layout.tsx
+++ unitas/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Unitas Paper",
+  title: "Unitas Papers",
   description: "Graphic and packing paper wholesaler",
 };
 
